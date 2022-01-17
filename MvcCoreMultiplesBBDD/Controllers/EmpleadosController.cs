8c82efa383b798f6338061027c2c24140e8a85bb
Steven-Cadena <steven.cadena.giler@gmail.com>
2022-01-17 14:06:39 +0100

    Add project files.

---
+++ MvcCoreMultiplesBBDD/Controllers/EmpleadosController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreMultiplesBBDD.Models;
+using MvcCoreMultiplesBBDD.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreMultiplesBBDD.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleados repo;
+        public EmpleadosController(RepositoryEmpleados repo) 
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            List<Empleado> empleados = this.repo.GetEmpleados();
+            return View(empleados);
+        }
+        public IActionResult Details(int id) 
+        {
+            Empleado empleado = this.repo.FindEmpleado(id);
+            return View(empleado);
+        }
+
+        public IActionResult Delete(int id) 
+        {
+            this.repo.DeleteEmpleado(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult UpdateSalario(int id) 
+        {
+            Empleado empleado = this.repo.FindEmpleado(id);
+            return View(empleado);
+        }
+        [HttpPost]
+        public IActionResult UpdateSalario(int idempleado, int incremento) 
+        {
+
+            return View();        
+        }
+    }
+}
---
+++ MvcCoreMultiplesBBDD/Startup.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MvcCoreMultiplesBBDD.Data;
+using MvcCoreMultiplesBBDD.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreMultiplesBBDD
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            /*********************IMPORTANTE************************/
+            string cadenaoracle = this.Configuration.GetConnectionString("hospitaloracle");
+            string cadenasqlserver = this.Configuration.GetConnectionString("hospitalsqlserver");
+            services.AddTransient<RepositoryEmpleados>();
+            //ACCESO A DATOS SQL SERVER *********************
+            services.AddDbContext<HospitalContext>(options => options.UseSqlServer(cadenasqlserver));
+
+            //esta solucion es modificando el find details el filtro
+            //services.AddDbContext<HospitalContext>(options => options.UseOracle(cadenaoracle));
+            //SOLUCION PARA ORACLE CON EL FIND DETAILS, sustituyendo la de arriba es mejor esta solucion
+            //esto es para ORACLE*************************************
+            /*services.AddDbContext<HospitalContext>
+            (options => options.UseOracle(cadenaoracle, options => options
+            .UseOracleSQLCompatibility("11")));*/
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
